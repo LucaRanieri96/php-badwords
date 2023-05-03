f88abe6e66dd4a84ea65f5c0ff39da7bb206bc5d
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-03 14:35:43 +0200

    Init  php-badwords

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Censura</title>
+</head>
+
+<body>
+  <form action="" method="post">
+    <label for="paragraph">Scrivi un paragrafo</label>
+    <textarea id="paragraph" name="paragraph" rows="4" cols="50" placeholder="At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.">
+    </textarea>
+    <label for="word">Inserisci la parola da censurare:</label>
+    <input type="text" id="word" name="word">
+    <input type="submit" value="censura">
+  </form>
+</body>
+
+</html>
