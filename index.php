5262d3b84fd5ef333d65887c021fd02a26d3164e
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-03 15:28:01 +0200

    Add bootstrap for better style

--- index.php
+++ index.php
@@ -5,17 +5,29 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
   <title>Censura</title>
 </head>
 
 <body>
-  <form action="myscript.php" method="post">
-    <label for="paragraph">Scrivi un paragrafo</label><br>
-    <textarea id="paragraph" name="paragraph" rows="4" cols="50"></textarea>
-    <label for="word">Inserisci la parola da censurare:</label><br>
-    <input type="text" id="word" name="word"><br>
-    <input type="submit" value="censura">
-  </form>
+  <div class="container">
+    <form action="myscript.php" method="post">
+      <label for="paragraph">Scrivi un paragrafo:</label>
+      <div>
+        <textarea id="paragraph" name="paragraph" rows="8" cols="100">Il paragrafo (o capoverso) è innanzitutto una porzione di testo formata da uno o più periodi e isolata da ciò che precede e ciò che segue. All’interno del paragrafo sono raggruppate porzioni di informazione omogenee, perciò il passaggio a un nuovo capoverso (il cosiddetto a capo) implica una pausa molto forte nel testo.</textarea>
+      </div>
+      <div>
+        <label for="word">Inserisci la parola da censurare:</label>
+        <input type="text" id="word" name="word">
+      </div>
+      <input type="submit" value="censura">
+    </form>
+
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
--- myscript.php
+++ myscript.php
@@ -14,21 +14,26 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
-  <div>
-    <h2>Paragrafo da censurare</h2>
-    <p><?php echo $paragraph; ?></p>
+  <div class="container">
+    <div>
+      <h2>Paragrafo da censurare</h2>
+      <p><?php echo $paragraph; ?></p>
+    </div>
+    <div>
+      <span>Parola da censurare: </span>
+      <span><?php echo $censoredWord; ?></span>
+    </div>
+    <div>
+      <h2>Paragrafo censurato</h2>
+      <p><?php echo $newParagraph; ?></p>
+    </div>
   </div>
-  <div>
-    <span>Parola da censurare: </span>
-    <span><?php echo $censoredWord; ?></span>
-  </div>
-  <div>
-    <h2>Paragrafo censurato</h2>
-    <p><?php echo $newParagraph; ?></p>
-  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
